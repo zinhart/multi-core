582a236030c8244b56ab733298a9ea39ae26640c
zinhart <azdegrac@ucsc.edu>
2018-04-19 01:05:00 -0700

    Added build scripts and macros header for cuda macros

--- include/concurrent_routines/concurrent_routines.hh
+++ include/concurrent_routines/concurrent_routines.hh
@@ -1,10 +1,16 @@
 #ifndef CONCURRENT_ROUNTINES_HH
 #define CONCURRENT_ROUNTINES_HH
+#include "macros.hh"
 #include <cstdint>
+//CPU ROUTINES
 void saxpy(const std::uint32_t n_elements, const double a, double * x, double * y);
 void launch_cpu_threaded_saxpy(
 		const std::uint32_t n_elements, const std::uint32_t n_threads,
 		const double a, double * x, double * y
 		);
+//GPU ROUTINES
+#if CUDA_ENABLED == true
 void launch_gpu_threaded_saxpy(const std::uint32_t n_elements, double a, double * x, double * y);
+#endif
+
 #endif
---
+++ include/concurrent_routines/macros.hh
@@ -0,0 +1,21 @@
+#ifndef MACROS_HH
+#define MACROS_HH
+
+//CUDACC is defined by nvcc
+#ifdef __CUDACC__
+#define CUDA_ENABLED true
+#define CUDA_CALLABLE_MEMBER __host__ __device__
+#define HOST __host__
+#define DEVICE __device__
+#define CONSTANT __constant__
+#define SHARED __shared_
+#else
+#define CUDA_ENABLED false
+#define CUDA_CALLABLE_MEMBER
+#define HOST
+#define DEVICE
+#define CONSTANT
+#define SHARED
+#endif
+
+#endif
